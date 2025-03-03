a4b385b468cc5b15442c905bb407c629b68d4eaf
Leonardo Matos <leonardolucas2020@gmail.com>
2025-03-02 22:20:02 -0400

    :recycle: refactoring: Initialize layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-zinc-900 text-zinc-100">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
