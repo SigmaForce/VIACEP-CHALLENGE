1eace0e64c0f6f11aa0f66b16f889e5b99e38a85
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-03-03 10:05:34 -0400

    :recycle: refactoring: use `FindZipCodeForm` component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/Button";
-import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
+import { FindZipCodeForm } from "./_components/FindZipCodeForm";
 
 export default function Home() {
   return (
     <div className="container mx-auto flex items-center justify-center min-h-dvh">
-      <div className="space-y-3 max-w-2xl">
+      <div className="space-y-2 max-w-2xl">
         <h1 className="text-4xl leading-normal">
           🔍 Descubra Tudo Sobre Seu CEP em Segundos!
         </h1>
@@ -12,15 +11,7 @@ export default function Home() {
           Digite seu CEP e tenha acesso imediato a informações detalhadas sobre
           sua localização.
         </span>
-
-        <div className="flex gap-2 w-full mt-4">
-          <input type="text" placeholder="Digite um CEP" className="w-full" />
-
-          <Button className="flex gap-2 ">
-            <MagnifyingGlass size={20} weight="bold" />
-            Buscar
-          </Button>
-        </div>
+        <FindZipCodeForm />
       </div>
     </div>
   );
